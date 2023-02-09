8a4d6c9c229182d8db3afc47dab4929eea80e0c7
Alberto Del Buono Paolini <alberto.delbuonopaolini@stud.unifi.it>
2023-02-09 21:13:49 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "theme.config.tsx",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
---
+++ theme.config.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
+
+const config: DocsThemeConfig = {
+  logo: (
+    <span className="font-bold md:text-lg transition-all">
+      <span className="md:mr-1">🌌</span> create-universal-app
+    </span>
+  ),
+  project: {
+    link: "https://github.com/chen-rn/CUA",
+  },
+  chat: {
+    link: "https://discord.gg/5HvtckjyYb",
+  },
+  docsRepositoryBase: "https://github.com/shuding/nextra-docs-template",
+  footer: {
+    component: () => (
+      <div className="text-center py-5 pb-5 border-t border-gray-800/70 text-gray-500">
+        Built with Nextra and NextJS by{" "}
+        <a
+          href="https://github.com/albbus-stack"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="underline text-gray-600 dark:text-gray-400"
+        >
+          albbus-stack
+        </a>
+      </div>
+    ),
+  },
+};
+
+export default config;
